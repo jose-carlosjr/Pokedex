1800e8d4b37c99283a3c24e81fc99d3da493e761
Jose <josecarlos099916@gmail.com>
2024-01-31 18:57:37 -0300

    Criei as funções dos botões Prev e Next

--- js/script.js
+++ js/script.js
@@ -5,29 +5,42 @@ const pokemonImage = document.querySelector('.pokemon__image')
 const form = document.querySelector('.form')
 const input = document.querySelector('.input__search')
 
+const buttonPrev = document.querySelector('.btn-prev')
+const buttonNext = document.querySelector('.btn-next')
+
+// VARIÁVEL QUE INCREMENTA E DECREMENTA O ID DO POKÉMON PARA AS FUNÇÕES DOS BOTÕES NEXT > E < PREV
+let searchPokemon = 1
+
 // FUNÇÃO QUE BUSCA OS DADOS DA API
 const fetchPokemon = async (pokemon) => {
-    const APIresponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`)
+    const APIresponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
 
     if (APIresponse.status === 200) {
         const data = await APIresponse.json()
         return data
-    } else {
-        alert('Not Found')
     }
 }
 
 // FUNÇÃO QUE RENDERIZA OS DADOS DO POKÉMON NA TELA
 const renderPokemon = async (pokemon) => {
+    pokemonNumber.innerText = ''
+    pokemonName.innerText = 'Loading...'
+
     const data = await fetchPokemon(pokemon)
 
     // CASO O POKÉMON DIGITADO NÃO EXISTA O data DEVE SER 'undefined', ENTÃO COLOCAMOS TUDO DENTRO DE UMA CONDIÇÃO. CASO O data SE CONFIRME (OU SEJA, CASO O PKM EXISTA) SEGUE A FUNÇÃO. SENÃO, NÃO FAZ NADA.
     if (data) {
+        pokemonImage.style.display = 'block'
         pokemonNumber.innerText = data.id + ' -'
         pokemonName.innerText = data.name
         // EVITAR PASSAR OS OBJETOS ATRAVÉS DE PONTOS QUANDO OS MESMOS TIVEREM HÍFEN, PRA EVITAR ERRO
         // EXEMPLO: data.generation-v | data['generation-v']
         pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+        searchPokemon = data.id
+    } else {
+        pokemonImage.style.display = 'none'
+        pokemonName.innerText = 'Not Found'
+        pokemonNumber.innerText = '??? -'
     }
 
     input.value = ''
@@ -39,5 +52,21 @@ const renderPokemon = async (pokemon) => {
 form.addEventListener('submit', (event) => {
     // PREVINE O COMPORTAMENTO PADRÃO DO FORMULÁRIO
     event.preventDefault()
-    renderPokemon(input.value)
+    renderPokemon(input.value.toLowerCase())
 })
+
+buttonPrev.addEventListener('click', () => {
+    if (searchPokemon > 1) {
+        searchPokemon -= 1
+        renderPokemon(searchPokemon)
+    }
+})
+
+buttonNext.addEventListener('click', () => {
+    if (searchPokemon < 1025) {
+        searchPokemon += 1
+        renderPokemon(searchPokemon)
+    }
+})
+
+renderPokemon(searchPokemon)
